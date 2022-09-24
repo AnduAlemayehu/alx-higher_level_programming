faa50fd6fac686f215b7185419af5bdcc6f953f8
Andu <andualemalemayehuwku@gmail.com>
2022-09-24 10:03:35 +0000

    all about structure

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+# 6-print_matrix_integer.py
+
+# Andu Alemayehu <andualemalemayehuwku@gmail.com>
+
+
+
+def print_matrix_integer(matrix=[[]]):
+
+    """Print a matrix of integers."""
+
+    for i in range(len(matrix)):
+
+        for j in range(len(matrix[i])):
+
+                print("{:d}".format(matrix[i][j]), end="")
+
+                if j != (len(matrix[i]) - 1):
+
+                    print(" ", end="")
+
+
+
+        print("")
---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+# 9-max_integer.py
+
+# Andu Alemayehu <andualemalemayehuwku@gmail.com>m>
+
+
+
+
+
+def max_integer(my_list=[]):
+
+    """Find the biggest integer of a list."""
+
+    if len(my_list) == 0:
+
+        return (None)
+
+
+
+    big = my_list[0]
+
+    for i in range(len(my_list)):
+
+        if my_list[i] > big:
+
+            big = my_list[i]
+
+
+
+    return (big)
