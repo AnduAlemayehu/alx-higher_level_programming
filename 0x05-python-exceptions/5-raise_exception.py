82afcdab915fbadc4a58dd5aad9095b5af3f64b6
Andu <andualemalemayehuwku@gmail.com>
2022-10-10 21:25:48 +0000

    all about expcetion

---
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -0,0 +1,17 @@
+Andu Alemayehu
+
+#!/usr/bin/python3
+
+# 5-raise_exception.py
+
+# Andu Alemayehu
+
+
+
+
+
+def raise_exception():
+
+    """Raise a TypeError exception."""
+
+    raise TypeError
