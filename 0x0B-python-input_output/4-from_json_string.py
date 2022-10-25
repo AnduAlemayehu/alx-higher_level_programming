6b5336252590c3578de5ee13d409270c140d5837
Andu <andualemalemayehuwku@gmail.com>
2022-10-25 22:12:44 +0000

    all about inouput

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+# 6-from_json_string.py
+
+# Andu
+
+"""Defines a JSON-to-object function."""
+
+import json
+
+
+
+
+
+def from_json_string(my_str):
+
+    """Return the Python object representation of a JSON string."""
+
+    return json.loads(my_str)
