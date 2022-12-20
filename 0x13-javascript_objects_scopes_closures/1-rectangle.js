ed20da1e3968362aa6770c083b1954ad47fb584e
Andu <andualemalemayehuwku@gmail.com>
2022-12-20 13:29:02 -0800

    javascript class

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+
+
+// Defines a Rectangle class with width and height attributes.
+
+
+
+module.exports = class Rectangle {
+
+  constructor (width, height) {
+
+    this.width = width;
+
+    this.height = height;
+
+  }
+
+};
