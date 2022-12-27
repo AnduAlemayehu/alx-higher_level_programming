2e48615e80f797282eeb57df392bf3151886ae64
Andu <andualemalemayehuwku@gmail.com>
2022-12-27 06:10:33 -0800

    fix database

--- 0x0F-python-object_relational_mapping/tests/6-model_state.sql
+++ 0x0F-python-object_relational_mapping/tests/6-model_state.sql
@@ -1,25 +1,7 @@
-#!/usr/bin/python3
+-- Create database hbtn_0e_6_usa
 
-"""Start link class to table in database
+CREATE DATABASE IF NOT EXISTS hbtn_0e_6_usa;
 
-"""
+USE hbtn_0e_6_usa;
 
-
-
-import sys
-
-from model_state import Base, State
-
-from sqlalchemy import (create_engine)
-
-
-
-if __name__ == "__main__":
-
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-
-                           pool_pre_ping=True)
-
-    Base.metadata.create_all(engine)
+SHOW CREATE TABLE states;
